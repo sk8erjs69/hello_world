eb0729ba68c69c70b4a35698c2a26f265d206e00
Greg <gpgag@comcast.net>
2015-09-09 14:44:20 -0400

    Changed For Exercise
    Changed from saying "Hello Class" to asking the user for their name,
    saying hello to them, and introducing myself.

--- hello.cpp
+++ hello.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main () {
   
-  cout << "Hello Class!\n\n";
+  string username;	
+
+  cout << "Enter your first name here: ";
+  cin >> username;
+  cout << "Hello " << username << ", my name is Greg";
 
   return 0;
 }
